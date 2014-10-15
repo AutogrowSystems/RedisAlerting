95a904763e8fb1b92bf04dd2fd58799df8e2f7f2
Robert McLeod <robert@autogrow.com>
2014-10-16 12:28:30 +1300

    made the executable use the alerting module

--- lib/redis/alerting.rb
+++ lib/redis/alerting.rb
@@ -1,7 +1,12 @@
 require "redis/alerting/version"
+require 'redis'
+require 'yaml'
 
 module Redis
   module Alerting
-    # Your code goes here...
+    def run(opts)
+      engine = Redis::Alerting::Engine.new(opts, Redis.new)
+      engine.run
+    end
   end
 end
