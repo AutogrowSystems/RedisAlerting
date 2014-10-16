d652f90e5a24d7c24269d55ca5cf71fb70d4bee0
Robert McLeod <robert@autogrow.com>
2014-10-16 18:58:21 +1300

    made sure that relative config file paths could be used

--- lib/redis_alerting/config.rb
+++ lib/redis_alerting/config.rb
@@ -12,8 +12,18 @@ module RedisAlerting
     private
 
     def parse_config
-      raise ArgumentError, "Invalid config file: #{config[:config]}" unless File.exists? @config[:config]
-      @config.merge(YAML.load_file(@config[:config])["alerting"])
+      raise ArgumentError, "No config file specified" if @config[:config].nil?
+      
+      # automatically use a relative config path
+      if @config[:config][0] != "/"
+        @config[:config] = File.expand_path(@config[:config], @config[:pwd])
+      end
+
+      raise ArgumentError, "Invalid config file: #{@config[:config]}" unless File.exists? @config[:config]
+      
+      yaml = YAML.load_file(@config[:config])
+      @config.merge!(yaml["alerting"])
+
       raise ArgumentError, "Incomplete configuration" unless valid_config?
     end
 
