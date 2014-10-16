a06ae72406dc1589f11dd32ab008f0b37b07c17a
Robert McLeod <robert@autogrow.com>
2014-10-16 14:19:45 +1300

    fixed by with alerting module

--- lib/redis_alerting.rb
+++ lib/redis_alerting.rb
@@ -5,14 +5,13 @@ require 'redis'
 require 'yaml'
 
 module RedisAlerting
-    def run(opts)
-      config = RedisAlerting::Config.new(opts).to_hash
-      engine = RedisAlerting::Engine.new(config, ::Redis.new)
+  def run(opts)
+    config = RedisAlerting::Config.new(opts).to_hash
+    engine = RedisAlerting::Engine.new(config, ::Redis.new)
 
-      loop do
-        engine.run
-        sleep config[:interval]
-      end
+    loop do
+      engine.run
+      sleep config[:interval]
     end
   end
 end
